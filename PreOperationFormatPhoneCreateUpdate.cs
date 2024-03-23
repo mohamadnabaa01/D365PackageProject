94b2c0398ad775374247e98fbd98c9941fa4a692
mohamadnabaa01 <mohamadnabaa01@lau.edu>
2024-03-23 20:04:41 +0200

    Add project files.

---
+++ PreOperationFormatPhoneCreateUpdate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using System.Text.RegularExpressions;
+
+namespace D365PackageProject
+{
+    public class PreOperationFormatPhoneCreateUpdate : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context =
+                (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            if (!context.InputParameters.ContainsKey("Target"))
+                throw new InvalidPluginExecutionException("No target found");
+
+            var entity = context.InputParameters["Target"] as Entity;
+
+            if (!entity.Attributes.Contains("telephone1"))
+                return;
+
+            string phoneNumber = (string)entity["telephone1"];
+            var formattedNumber = Regex.Replace(phoneNumber, @"[^\d]", "");
+
+            entity["telephone1"] = formattedNumber;
+        }
+    }
+}
